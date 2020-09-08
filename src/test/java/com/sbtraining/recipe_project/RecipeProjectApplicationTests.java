90b0ab874067254c1a48b77e8ef8e3e133e3b58b
sousajo <josesousa@rentalcars.com>
2020-09-08 12:36:33 +0100

    initial commit

---
+++ src/test/java/com/sbtraining/recipe_project/RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbtraining.recipe_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
