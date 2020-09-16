57c1fa7bf279f9fc4b01f207bbecdc293e011c63
sousajo <josesousa@rentalcars.com>
2020-09-16 23:41:22 +0100

    created UnitOfMeasure and defined relations UnitOfMeasure and Ingredient. Closes #8

---
+++ src/main/java/com/sbtraining/recipe_project/model/UnitOfMeasure.java
@@ -0,0 +1,36 @@
+package com.sbtraining.recipe_project.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by sousaJ on 16/09/2020
+ * in package - com.sbtraining.recipe_project.model
+ **/
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String uom) {
+        this.description = uom;
+    }
+}
