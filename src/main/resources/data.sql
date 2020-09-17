02ba52e39f5deb824ca382689cdefc9d1115de04
sousajo <josesousa@rentalcars.com>
2020-09-17 21:33:49 +0100

    Added data initializer for Category and UnitOfMeasure. Closes #13

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO category (category_name) VALUES('American');
+INSERT INTO category (category_name) VALUES('Italian');
+INSERT INTO category (category_name) VALUES('Mexican');
+INSERT INTO category (category_name) VALUES('Fast Food');
+INSERT INTO category (category_name) VALUES('Portuguese');
+INSERT INTO unit_of_measure (description) VALUES('Teaspoon');
+INSERT INTO unit_of_measure (description) VALUES('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES('Cup');
+INSERT INTO unit_of_measure (description) VALUES('Pinch');
+INSERT INTO unit_of_measure (description) VALUES('Ounce');
+INSERT INTO unit_of_measure (description) VALUES('Gram');
+INSERT INTO unit_of_measure (description) VALUES('Kilogram');
+INSERT INTO unit_of_measure (description) VALUES('Liter');
+INSERT INTO unit_of_measure (description) VALUES('Deciliter');
+INSERT INTO unit_of_measure (description) VALUES('Centiliter');
+
