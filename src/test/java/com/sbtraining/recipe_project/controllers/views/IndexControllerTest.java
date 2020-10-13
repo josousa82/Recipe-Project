b1e25b3e1118a4d8d569a4f6c2912142de6093e9
sousajo <josesousa@rentalcars.com>
2020-10-13 16:49:36 +0100

    Refactored test of IndexController.getIndexPage() to test in a BDD way. Tested not only interactions but content from model as well using argumentCaptor, and tests more in the semantics of spock framework. . Closes #39

--- src/test/java/com/sbtraining/recipe_project/controllers/views/IndexControllerTest.java
+++ src/test/java/com/sbtraining/recipe_project/controllers/views/IndexControllerTest.java
@@ -1,15 +1,28 @@
 package com.sbtraining.recipe_project.controllers.views;
 
+import com.sbtraining.recipe_project.model.Recipe;
 import com.sbtraining.recipe_project.services.RecipeServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.ui.Model;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class IndexControllerTest {
 
     // Under Test
@@ -29,12 +42,25 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        // given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
 
-        String index = "index";
-        assertEquals(indexController.getIndexPage(model), index);
+        when(recipeService.getAllRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        // when
+        String viewName = indexController.getIndexPage(model);
+
+        // then
+        assertEquals("index", viewName);
 
         // interactions tests
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
         verify(recipeService, times(1)).getAllRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        log.warn("Check that 2 recipes are at test" + Arrays.toString(setInController.toArray()));
+        assertEquals(2, setInController.size());
     }
 }
