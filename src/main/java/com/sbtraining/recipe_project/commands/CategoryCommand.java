c67b8112428047eaca5e9c20a187e183d0330c83
sousajo <josesousa@rentalcars.com>
2020-11-05 02:38:12 +0000

    WIP adding webjars. WIP 49

---
+++ src/main/java/com/sbtraining/recipe_project/commands/CategoryCommand.java
@@ -0,0 +1,8 @@
+package com.sbtraining.recipe_project.commands;
+
+/**
+ * Created by sousaJ on 04/11/2020
+ * in package - com.sbtraining.recipe_project.commands
+ **/
+public class CategoryCommand {
+}
