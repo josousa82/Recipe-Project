36f06f7f6fc065abae2330501c55d784a5e49875
sousajo <josesousa@rentalcars.com>
2020-11-18 19:12:15 +0000

    Bug solved and code refactored. Closes #64

--- src/test/java/com/sbtraining/recipe_project/repositories/UnitOfMeasureRepositoryTestIT.java
+++ src/test/java/com/sbtraining/recipe_project/repositories/UnitOfMeasureRepositoryTestIT.java
@@ -3,18 +3,15 @@ package com.sbtraining.recipe_project.repositories;
 import com.sbtraining.recipe_project.model.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ExtendWith(SpringExtension.class)
-@DataJpaTest
+@SpringBootTest
 class UnitOfMeasureRepositoryTestIT {
 
     @Autowired
