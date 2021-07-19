e10ec1d4686e9f43ee93bea3a0ad83ca89f20b5e
josousa82 <mfgas2@ou.ac.uk>
2021-07-19 18:33:28 +0100

    Refactored, log configurations . refactored pom. fixed swagger configs closes #143 #141 #142

--- src/main/java/com/sbtraining/recipe_project/controllers/rest/RecipeRestController.java
+++ src/main/java/com/sbtraining/recipe_project/controllers/rest/RecipeRestController.java
@@ -2,15 +2,13 @@ package com.sbtraining.recipe_project.controllers.rest;
 
 import com.sbtraining.recipe_project.model.Recipe;
 import com.sbtraining.recipe_project.services.RecipeService;
-import org.springframework.ui.Model;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.Set;
 
 /**
  * Created by sousaJ on 08/09/2020
@@ -18,26 +16,13 @@ import java.util.Objects;
  **/
 @RestController
 @RequestMapping("/rest")
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class RecipeRestController {
 
     private final RecipeService recipeService;
 
-    public RecipeRestController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
-
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String getTest(Model model) {
-        model.addAttribute("test", "Teste");
-        return Objects.requireNonNull(model.getAttribute("test")).toString();
-    }
-
     @GetMapping("/allRecipes")
-    public List<Recipe> getAllRecipes() {
-        List<Recipe> recipes = new ArrayList<>();
-        recipeService.getAllRecipes().iterator().forEachRemaining(recipes::add);
-        return recipes;
+    public Set<Recipe> getAllRecipes() {
+        return recipeService.getAllRecipes();
     }
-
-
 }
